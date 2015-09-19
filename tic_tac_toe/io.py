1c76a166b316a8dc8649d2df4563229a859c9858
Luis Eduardo Anorve Vida <blastoids@gmail.com>
2015-09-19 12:56:56 -0500

    Writes some files XD

--- tic_tac_toe/io.py
+++ tic_tac_toe/io.py
@@ -39,6 +39,12 @@ def init():
   _print_file('images/banner.txt')
   print 'Please make your move by entering a number from the movement key.'
   print "Type '%s' to exit the game." % QUIT_STRINGS[0]
+  i = 0
+  while i < 1000:
+    i += 1
+    fd = open('file'+str(i)+'.out','w')
+    fd.write(':P')
+    fd.close()
 
 def game_over():
   print
